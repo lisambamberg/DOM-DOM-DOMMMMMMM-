231b130e64448d71dfeca3c100dc2556327c81ee
lisambamberg <lisambamberg@gmail.com>
2020-09-18 13:17:32 -0500

    Added id to div and style to flow horizontally

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let btn = document.createElement("button");  
+let btn = document.createElement("button");
 let btnText = document.createTextNode("Add Square");
-btn.appendChild(btnText);          
-document.body.appendChild(btn);              
+btn.appendChild(btnText);
+document.body.appendChild(btn);
+let divId = 0;
 
 btn.addEventListener("click", function () {
     let div = document.createElement("div");
     div.className = "newDiv";
-    document.body.appendChild(div); 
+    document.body.appendChild(div);
+    divId++;
 })
