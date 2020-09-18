6d38dc2f255ffb69a3819bf4e21e7a889ec20cba
lisambamberg <lisambamberg@gmail.com>
2020-09-18 15:42:26 -0500

    Added mouseover and mouseout for divs

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-let btn = document.createElement("button");
-let btnText = document.createTextNode("Add Square");
-btn.appendChild(btnText);
-document.body.appendChild(btn);
-let divId = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.createElement("button");
+    let btnText = document.createTextNode("Add Square");
+    btn.appendChild(btnText);
+    document.body.appendChild(btn);
+    let divId = 0;
 
-btn.addEventListener("click", function () {
-    let div = document.createElement("div");
-    div.className = "newDiv";
-    document.body.appendChild(div);
-    divId++;
-    div.appendChild(document.createTextNode(divId));
-})
+    let squareContainer = document.createElement("div");
+    squareContainer.className = "container";
+    document.body.appendChild(squareContainer);
+
+    btn.addEventListener("click", function () {
+        let div = document.createElement("div");
+        div.className = "newDiv";
+        squareContainer.appendChild(div);
+        let valueSquareId = document.createElement("span");
+        valueSquareId.className = "divId";
+        valueSquareId.innerText = divId;
+        div.appendChild(valueSquareId);
+        divId++;
+
+        div.addEventListener("mouseover", function () {
+            valueSquareId.style.display = "inline";
+        });
+
+        div.addEventListener("mouseout", function () {
+            valueSquareId.style.display = "none";
+        });
+    });
+});
